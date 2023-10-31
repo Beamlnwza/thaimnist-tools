ac84e011f032b72511f789d6402f34862d8ef884
Beamlnwza <Supawit04591@pcccr.ac.th>
2023-10-31 16:14:42 +0700

    tools: better painter and pallete to constanly check contrast brightness and threshould

--- tools/painter.py
+++ tools/painter.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from PIL import Image, ImageTk, ImageDraw, ImageEnhance, ImageFilter, ImageOps
+from PIL import Image, ImageTk, ImageDraw, ImageEnhance, ImageOps
 import PIL.ImageOps
 import os
 
@@ -7,33 +7,41 @@ import os
 class ImagePainter:
     def __init__(self, master, image_path, output_path):
         self.master = master
+        self.master.title("Image Painter")
+        self.brush_size = 40
 
-        threshold = 230
-
-        self.image = Image.open(image_path)
-
-        self.image = ImageEnhance.Brightness(self.image).enhance(0.962)
-        self.image = ImageEnhance.Contrast(self.image).enhance(0.5)
-
-        self.image = self.image.convert("L")
-        self.image = self.image.point(lambda p: 255 if p > threshold else 0)
-        self.image = self.image.convert("1")
-
-        self.image = PIL.ImageOps.invert(self.image)
+        self.brightness = 0.9
+        self.contrast = 2.0
+        self.threshold = 191
 
         self.zoom_percentage = 350
 
-        self.image = self.image.resize(
-            (
-                int(self.image.width * self.zoom_percentage / 100),
-                int(self.image.height * self.zoom_percentage / 100),
-            )
-        )
+        self.image = self.load_and_process_image(image_path)
 
         self.output_path = output_path
 
+        self.setup_canvas()
+
+    def load_and_process_image(self, image_path):
+        image = Image.open(image_path)
+        image = image.convert("L")
+        image = ImageEnhance.Brightness(image).enhance(self.brightness)
+        image = ImageEnhance.Contrast(image).enhance(self.contrast)
+        image = image.point(lambda p: 255 if p > self.threshold else 0)
+
+        image = image.convert("1")
+        image = PIL.ImageOps.invert(image)
+        image = image.resize(
+            (
+                int(image.width * self.zoom_percentage / 100),
+                int(image.height * self.zoom_percentage / 100),
+            )
+        )
+        return image
+
+    def setup_canvas(self):
         self.canvas = tk.Canvas(
-            master, width=self.image.width, height=self.image.height
+            self.master, width=self.image.width, height=self.image.height
         )
         self.canvas.pack()
 
@@ -41,71 +49,71 @@ class ImagePainter:
         self.canvas.create_image(0, 0, anchor=tk.NW, image=self.tk_image)
 
         self.canvas.bind("<Motion>", self.paint)
-
         self.master.bind("<Return>", self.save)
 
     def paint(self, event):
         x, y = event.x, event.y
-        r = 30
+        r = self.brush_size
         draw = ImageDraw.Draw(self.image)
         draw.ellipse((x - r, y - r, x + r, y + r), fill="black")
         self.tk_image = ImageTk.PhotoImage(self.image)
         self.canvas.create_image(0, 0, anchor=tk.NW, image=self.tk_image)
 
     def save(self, event):
-        # resize back
-
+        # Your existing code...
+        if not os.path.exists(os.path.dirname(self.output_path)):
+            os.makedirs(os.path.dirname(self.output_path))
         self.image = self.image.resize(
             (
                 int(self.image.width * 100 / self.zoom_percentage),
                 int(self.image.height * 100 / self.zoom_percentage),
             )
         )
-
-        self.image = make_white_to_black(self.image)
-        self.image = self.image.convert("1")
+        self.image = self.image.convert("L")
+        self.image = self.image.point(lambda p: 0 if p < self.threshold else 255)
         self.image.save(self.output_path)
         print("Saved to {}".format(self.output_path))
         self.master.destroy()
 
-
-def make_white_to_black(image):
-    width, height = image.size
-
-    for y in range(height):
-        for x in range(width):
-            pixel = image.getpixel((x, y))
-            if pixel == (255, 255, 255):
-                neighbors = []
-
-                if x > 0:
-                    neighbors.append(image.getpixel((x - 1, y)))
-                if x < width - 1:
-                    neighbors.append(image.getpixel((x + 1, y)))
-                if y > 0:
-                    neighbors.append(image.getpixel((x, y - 1)))
-                if y < height - 1:
-                    neighbors.append(image.getpixel((x, y + 1)))
-                if all(n != (255, 255, 255) for n in neighbors):
-                    image.putpixel((x, y), (0, 0, 0))
-
-    return image
+    def make_white_to_black(self, image):
+        width, height = image.size
+        for y in range(height):
+            for x in range(width):
+                pixel = image.getpixel((x, y))
+                if pixel == (255, 255, 255):
+                    neighbors = []
+                    if x > 0:
+                        neighbors.append(image.getpixel((x - 1, y)))
+                    if x < width - 1:
+                        neighbors.append(image.getpixel((x + 1, y)))
+                    if y > 0:
+                        neighbors.append(image.getpixel((x, y - 1)))
+                    if y < height - 1:
+                        neighbors.append(image.getpixel((x, y + 1)))
+                    if all(n != (255, 255, 255) for n in neighbors):
+                        image.putpixel((x, y), (0, 0, 0))
+        return image
 
 
 if __name__ == "__main__":
-    root = tk.Tk()
-    root.state("zoomed")
-    image_path = "./box_data/00/067.jpg"
-    output_path = "001.jpg"
-
-    image_painter = ImagePainter(root, image_path, output_path)
-    root.mainloop()
-
-    """ main_path = "./box_data/04"
+    main_path = "./box_data/07"
     main_output_path = f"./uncenter_data/{main_path[-2:]}"
+
     for i in sorted(os.listdir(main_path)):
         path = os.path.join(main_path, i)
         output_path = os.path.join(main_output_path, i)
+        root = tk.Tk()
+
+        root.state("zoomed")
+
+        image_painter = ImagePainter(root, path, output_path)
+        root.mainloop()
+
+    # command to handle for in list [number, number...]
+
+    """ for i in [164, 292]:
+        path = os.path.join(main_path, f"{i}.jpg")
+        output_path = os.path.join(main_output_path, f"{i}.jpg")
         root = tk.Tk()
 
         root.state("zoomed")
---
+++ tools/pallete.py
@@ -0,0 +1,101 @@
+import tkinter as tk
+from PIL import Image, ImageTk, ImageEnhance, ImageOps
+import os
+import PIL
+
+
+class ImageProcessor:
+    def __init__(self, master, image_path):
+        self.master = master
+        self.master.title("Image Processor")
+        self.master.geometry("600x600")
+
+        self.brightness = 1.4
+        self.contrast = 1.5
+        self.threshold = 233
+
+        self.image = self.load_and_process_image(image_path)
+
+        self.setup_canvas()
+        self.setup_controls()
+
+    def load_and_process_image(self, image_path):
+        image = Image.open(image_path)
+        image = ImageEnhance.Brightness(image).enhance(self.brightness)
+        image = ImageEnhance.Contrast(image).enhance(self.contrast)
+        image = image.point(lambda p: 255 if p > self.threshold else 0)
+        image = PIL.ImageOps.invert(image)
+        return image
+
+    def setup_canvas(self):
+        self.canvas = tk.Canvas(
+            self.master, width=self.image.width, height=self.image.height
+        )
+        self.canvas.pack()
+
+        self.tk_image = ImageTk.PhotoImage(self.image)
+        self.canvas.create_image(0, 0, anchor=tk.NW, image=self.tk_image)
+
+    def setup_controls(self):
+        self.brightness_scale = tk.Scale(
+            self.master,
+            from_=0.1,
+            to=2,
+            resolution=0.1,
+            orient=tk.HORIZONTAL,
+            label="Brightness",
+            command=lambda value: self.adjust_brightness(float(value)),
+        )
+        self.brightness_scale.set(self.brightness)
+        self.brightness_scale.pack()
+
+        self.contrast_scale = tk.Scale(
+            self.master,
+            from_=0.1,
+            to=2,
+            resolution=0.1,
+            orient=tk.HORIZONTAL,
+            label="Contrast",
+            command=lambda value: self.adjust_contrast(float(value)),
+        )
+        self.contrast_scale.set(self.contrast)
+        self.contrast_scale.pack()
+
+        self.threshold_scale = tk.Scale(
+            self.master,
+            from_=0,
+            to=255,
+            orient=tk.HORIZONTAL,
+            label="Threshold",
+            command=lambda value: self.adjust_threshold(int(value)),
+        )
+        self.threshold_scale.set(self.threshold)
+        self.threshold_scale.pack()
+
+    def adjust_brightness(self, value):
+        self.brightness = value
+        self.image = self.load_and_process_image(image_path)
+        self.update_image()
+
+    def adjust_contrast(self, value):
+        self.contrast = value
+        self.image = self.load_and_process_image(image_path)
+        self.update_image()
+
+    def adjust_threshold(self, value):
+        self.threshold = value
+        self.image = self.load_and_process_image(image_path)
+        self.update_image()
+
+    def update_image(self):
+        self.tk_image = ImageTk.PhotoImage(self.image)
+        self.canvas.create_image(0, 0, anchor=tk.NW, image=self.tk_image)
+
+
+if __name__ == "__main__":
+    image_path = "./box_data/06/299.jpg"  # Replace with the path to your image
+    root = tk.Tk()
+    image_processor = ImageProcessor(root, image_path)
+    root.mainloop()
+
+    # 0.9 2.0 191
